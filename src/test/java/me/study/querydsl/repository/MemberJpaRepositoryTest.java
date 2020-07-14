7954ec74fa3524d3ee9c7e9ddbb8cf78a1382d6b
Jeongsu Yang <mycat83@gmail.com>
2020-07-14 22:46:21 +0900

    동적 쿼리와 성능 최적화 조회 - Builder 사용

--- src/test/java/me/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/me/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -1,6 +1,9 @@
 package me.study.querydsl.repository;
 
+import me.study.querydsl.dto.MemberSearchCondition;
+import me.study.querydsl.dto.MemberTeamDto;
 import me.study.querydsl.entity.Member;
+import me.study.querydsl.entity.Team;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,6 +26,7 @@ class MemberJpaRepositoryTest {
 
     @Test
     public void basicTest() {
+
         Member member = new Member("member1", 10);
         memberJpaRepository.save(member);
 
@@ -38,6 +42,7 @@ class MemberJpaRepositoryTest {
 
     @Test
     public void basicTest_Querydsl() {
+
         Member member = new Member("member1", 10);
         memberJpaRepository.save(member);
 
@@ -49,5 +54,32 @@ class MemberJpaRepositoryTest {
 
         List<Member> result2 = memberJpaRepository.findByUsername_Querydsl("member1");
         assertThat(result2).containsExactly(member);
+    }
+
+    @Test
+    public void searchTest() {
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+
+        assertThat(result).extracting("username").containsExactly("member4");
     }
 }
